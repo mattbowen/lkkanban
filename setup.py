f4af0ab1a39d622ab2a0a5ce8ecd8bfd690fef5c
mattbowen <mrbowen@gmail.com>
2012-07-08 16:09:33 -0400

    Added requests dependency

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0'
+version = '0.1'
 
 setup(name='lkkanban',
-      version=version,
-      description="",
-      long_description="""\
+    version=version,
+    description="",
+    long_description="""\
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='Matt Bowen',
-      author_email='None',
-      url='https://github.com/mattbowen/lkkanban',
-      license='',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Matt Bowen',
+    author_email='None',
+    url='https://github.com/mattbowen/lkkanban',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'requests',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+    )
